6dc219711246887aa11245768314961fa43794e8
Akira Komamura <akira.komamura@gmail.com>
2021-12-27 15:19:38 +0900

    feat: Add flake-no-path

--- flake.nix
+++ flake.nix
@@ -10,13 +10,15 @@
     inputs.nixpkgs.follows = "nixpkgs";
     inputs.flake-utils.follows = "flake-utils";
   };
+  inputs.flake-no-path.url = "github:akirak/flake-no-path";
 
   outputs =
     { self
     , nixpkgs
     , flake-utils
     , pre-commit-hooks
-    }:
+    , ...
+    } @ inputs:
     flake-utils.lib.eachDefaultSystem
       (system:
       let
@@ -31,6 +33,15 @@
           default = {
             nixpkgs-fmt.enable = true;
             nix-linter.enable = false;
+            flake-no-path = {
+              enable = true;
+              name = "Ensure that flake.lock does not contain a local path";
+              entry = "${
+                inputs.flake-no-path.packages.${system}.flake-no-path
+              }/bin/flake-no-path";
+              files = "flake\.lock$";
+              pass_filenames = true;
+            };
           };
         };
       in
